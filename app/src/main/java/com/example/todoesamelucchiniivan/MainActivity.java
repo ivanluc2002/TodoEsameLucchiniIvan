9dcd2d33ee1d1195ff92c5e2a6c61e093264c1f8
ivanluc2002 <103107740+ivanluc2002@users.noreply.github.com>
2023-01-17 12:51:48 +0100

    add inserisci azione

--- app/src/main/java/com/example/todoesamelucchiniivan/MainActivity.java
+++ app/src/main/java/com/example/todoesamelucchiniivan/MainActivity.java
@@ -6,27 +6,51 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 private RecyclerView recyclerView;
 private final FirebaseFirestore db=FirebaseFirestore.getInstance();
+private EditText inserisciAzione;
+private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=findViewById(R.id.recycler1);
+        inserisciAzione=findViewById(R.id.inserisciAzione);
+        button=findViewById(R.id.button);
         ArrayList<String> azioni= new ArrayList<>();
+        leggiDb(azioni);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDb(azioni);
+            }
+        });
 
 
+
+
+
+    }
+    public void leggiDb(ArrayList azioni){
         db.collection("todo")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -45,12 +69,21 @@ private final FirebaseFirestore db=FirebaseFirestore.getInstance();
                         } else {
                             Log.d("errore", "Error getting documents.", task.getException());
                         }
-                        TodoAdapter todoAdapter= new TodoAdapter(azioni.toArray(new String[0]));
+                        TodoAdapter todoAdapter= new TodoAdapter((String[]) azioni.toArray(new String[0]));
                         recyclerView.setAdapter(todoAdapter);
                     }
 
                 });
-
-
+    }
+    public void addDb(ArrayList azioni){
+        CollectionReference ref1= db.collection("todo");
+        Map<String,String> data=new HashMap<>();
+        data.put("azione",inserisciAzione.getText().toString());
+        ref1.add(data).addOnSuccessListener(documentReference -> {
+            Log.d("aggiunto", "addDb: "+documentReference.getId());
+            azioni.add(inserisciAzione.getText());
+        }).addOnFailureListener(e -> {
+            Log.w("errore", "addDb: ", e);
+        });
     }
 }
